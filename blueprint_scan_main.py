405d012bb3c9caf9173d21b25d4df332cc745bd4
Christopher <46629049+blamechris@users.noreply.github.com>
2022-11-05 14:24:30 -0700

    Update comment to explain bbox

--- blueprint_scan_main.py
+++ blueprint_scan_main.py
@@ -8,6 +8,6 @@ import pyscreenshot
 #           Process screenshot using tesseract
 #               Copy blueprint names from screenshot to clipboard for pasting into excel
 
-pic = pyscreenshot.grab(bbox=(0, 0, 1280, 1440)) # bbox = (X1,Y1,X2,Y2) I think these are the corners of the screen maybe?
+pic = pyscreenshot.grab(bbox=(0, 0, 1280, 1440)) # bbox = (X1,Y1,X2,Y2) X1,Y1 I think these are the corners of the screen maybe?
 pic.show()
 pic.save("ss.png") 
