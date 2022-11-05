10779838f8c7eac0999378fa788d96132a475abd
csovereignty <christopher.bowenx@gmail.com>
2022-11-05 14:22:41 -0700

    Setup Started, Screenshotting of half of screen implemented

---
+++ blueprint_scan_main.py
@@ -0,0 +1,13 @@
+import pytesseract
+import pyscreenshot
+
+# Outline:
+# On run
+#   Screenshot current game screen
+#       Output Screenshot to a file (Overwrite existing to save space)
+#           Process screenshot using tesseract
+#               Copy blueprint names from screenshot to clipboard for pasting into excel
+
+pic = pyscreenshot.grab(bbox=(0, 0, 1280, 1440)) # bbox = (X1,Y1,X2,Y2) I think these are the corners of the screen maybe?
+pic.show()
+pic.save("ss.png") 
