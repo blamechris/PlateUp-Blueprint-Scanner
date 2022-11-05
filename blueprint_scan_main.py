6aefd31a11ca14491019ac45297b40d78d4bc3b8
Christopher <46629049+blamechris@users.noreply.github.com>
2022-11-05 14:51:35 -0700

    Edit to pytesseract.py's tesseract_cmd to fix

--- blueprint_scan_main.py
+++ blueprint_scan_main.py
@@ -1,5 +1,10 @@
 import pytesseract
 import pyscreenshot
+import argparse
+import cv2
+
+
+
 
 # Outline:
 # On run
@@ -9,5 +14,15 @@ import pyscreenshot
 #               Copy blueprint names from screenshot to clipboard for pasting into excel
 
 pic = pyscreenshot.grab(bbox=(0, 0, 1280, 1440)) # bbox = (X1,Y1,X2,Y2) X1,Y1 I think these are the corners of the screen maybe?
-pic.show()
-pic.save("ss.png") 
+#pic.show()
+pic.save("ss.png")
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "path to input image for OCR")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+text = pytesseract.image_to_string(image)
+print(text)
