c64d15f2b696b8db28d1ae33d033fef39c3a1da9
Christopher <46629049+csovereignty@users.noreply.github.com>
2023-03-20 22:31:27 -0700

    Update to better use pytesseract (not tested)

--- blueprint_scan_main.py
+++ blueprint_scan_main.py
@@ -12,18 +12,36 @@ import cv2
 #           Process screenshot using tesseract
 #               Copy blueprint names from screenshot to clipboard for pasting into excel
 
-#pic = pyscreenshot.grab(bbox=(0, 100, 1280, 500)) # bbox = (X1,Y1,X2,Y2) X1,Y1 I think these are the corners of the screen maybe?
-#pic.show()
-#pic.save("ss.png")
 
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required = True, help = "path to input image for OCR")
-# args = vars(ap.parse_args())
+import pytesseract
+import pyscreenshot
+import argparse
+import cv2
+
+# Set pytesseract configuration parameters
+config = '--psm 6'
+
+# Capture screenshot
+bbox = (0, 100, 1280, 500)
+image = pyscreenshot.grab(bbox=bbox)
+image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+# Preprocess image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 3)
+gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+# Perform OCR
+text = pytesseract.image_to_string(gray, config=config)
+
+# Print OCR result
+print(text)
 
 
-# image = cv2.imread(args["image"])
+"""
+# old version
 image = cv2.imread("ss.png")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 text = pytesseract.image_to_string(image)
-print(text)
+print(text) """
